a2eccce16364fb1318830c09e8dccb25f4f59f9a
Sara <saraahmad1114@yahoo.com>
2016-07-08 09:51:37 -0400

    optional stuff

--- Pets.playground/Contents.swift
+++ Pets.playground/Contents.swift
@@ -11,8 +11,11 @@ if petName != nil {
 if let petName = petName {
     print("My friend's pet is \(petName)")
 }
+//Above you are unwrapping your optional and getting its value equal to something and inside the body of the if statement you can do whatever you like, but this variable petName or optional is going to be an optional outside this if statement and everywhere in the code 
 
 if let friendsPet = petName {
     print("My friend's pet is \(friendsPet)")
     print("petName is still an Optional! \(petName)")
 }
+
+
